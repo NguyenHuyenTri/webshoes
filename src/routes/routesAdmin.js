59056f8e0a31ae9abface9029145f0edab070c98
hieu2211 <h.vanhieu1999@gmail.com>
2021-01-29 15:01:18 +0700

    hiue pull code

--- src/routes/routesAdmin.js
+++ src/routes/routesAdmin.js
@@ -12,86 +12,94 @@ import "static/assets/scss/now-ui-dashboard.scss?v1.4.0";
 import AddProduct from "views/Admin/Product/AddProduct";
 import PageWoman from "../views/Admin/Page/Woman/PageWoman";
 import PageKid from "../views/Admin/Page/Kid/PageKid";
+import ListContact from "../views/Admin/Contact/ListContact";
 
 
-var dashRoutes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: "design_app",
-    component: Dashboard,
-    layout: "/admin",
-    display:true
-  },
+var dashRoutes = [{
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: "design_app",
+        component: Dashboard,
+        layout: "/admin",
+        display: true
+    },
     // product list
-  {
-    path: "/productlist",
-    name: "Product List",
-    icon: "design_bullet-list-67",
-    component: ProductList,
-    layout: "/admin",
-    display:true
-  },
+    {
+        path: "/productlist",
+        name: "Product List",
+        icon: "design_bullet-list-67",
+        component: ProductList,
+        layout: "/admin",
+        display: true
+    },
     // add product
-  {
-    path: "/addproduct",
-    name: "New Product",
-    icon: "ui-1_simple-add",
-    component: AddProduct,
-    layout: "/admin",
-    display:true,
-    nav:false
-  },
+    {
+        path: "/addproduct",
+        name: "New Product",
+        icon: "ui-1_simple-add",
+        component: AddProduct,
+        layout: "/admin",
+        display: true,
+        nav: false
+    },
 
-  {
-    path: "/brandname",
-    name: "Brand & Gender List",
-    icon: "objects_planet",
-    component: Maps,
-    layout: "/admin",
-    display:true,
-    nav:false,
-  },
-  {
-    path: "/pagehome",
-    name: "Page Home",
-    icon: "education_paper",
-    component: PageHome,
-    layout: "/admin",
-    display:true,
-  },
-  {
-    path: "/pagemen",
-    name: "Page Men",
-    icon: "education_paper",
-    component: PageMen,
-    layout: "/admin",
-    display:true,
-  },
-  {
-    path: "/pagewoman",
-    name: "Page Woman",
-    icon: "education_paper",
-    component: PageWoman,
-    layout: "/admin",
-    display:true,
-  },
-  {
-    path: "/pagekid",
-    name: "Page Kid",
-    icon: "education_paper",
-    component: PageKid,
-    layout: "/admin",
-    display:true,
-  },
-  {
-    path: "/update/:id",
-    name: "Update",
-    icon: "design-2_ruler-pencil",
-    component: AddProduct,
-    layout: "/admin",
-    display:false,
-    nav:false,
-  },
+    {
+        path: "/brandname",
+        name: "Brand & Gender List",
+        icon: "objects_planet",
+        component: Maps,
+        layout: "/admin",
+        display: true,
+        nav: false,
+    },
+    {
+        path: "/pagehome",
+        name: "Page Home",
+        icon: "education_paper",
+        component: PageHome,
+        layout: "/admin",
+        display: true,
+    },
+    {
+        path: "/pagemen",
+        name: "Page Men",
+        icon: "education_paper",
+        component: PageMen,
+        layout: "/admin",
+        display: true,
+    },
+    {
+        path: "/pagewoman",
+        name: "Page Woman",
+        icon: "education_paper",
+        component: PageWoman,
+        layout: "/admin",
+        display: true,
+    },
+    {
+        path: "/pagekid",
+        name: "Page Kid",
+        icon: "education_paper",
+        component: PageKid,
+        layout: "/admin",
+        display: true,
+    },
+    {
+        path: "/update/:id",
+        name: "Update",
+        icon: "design-2_ruler-pencil",
+        component: AddProduct,
+        layout: "/admin",
+        display: false,
+        nav: false,
+    },
+    {
+        path: "/Contact/ListContact",
+        name: "Contact",
+        icon: "education_paper",
+        component: ListContact,
+        layout: "/admin",
+        display: true,
+    },
 ];
 export default dashRoutes;
--- src/views/Website/Contact/Contact.js
+++ src/views/Website/Contact/Contact.js
@@ -1,35 +1,57 @@
-import React, { useState, useEffect }from "react";
+import React, { useRef,useState }from "react";
 import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
 import 'static/website/css/contact/contact.css'
+import NotificationAlert from "react-notification-alert";
 import { NavLink, Row, Col, Form, Button,Textarea,FormControl } from "react-bootstrap";
 
-function FormError(props) {
-  /* nếu isHidden = true, return null ngay từ đầu */
-  if (props.isHidden) { return null;}
-
-  return ( <div>{props.errorMessage}</div>)
-}
 export default function Contact() {
-  // const [value, isInputValid,errorMessage] = useState();
+  const notificationAlert =useRef(null);
+  const [show, setShow] = useState(false);
+  const Alert = (status,message) =>{
+    let options = {};
+    options = {
+        place: 'tr',
+        message: (
+            <div>
+                <div>
+                    {status} <b>{message}</b>
+                </div>
+            </div>
+        ),
+        type: 'success',
+        icon: "now-ui-icons ui-1_bell-53",
+        autoDismiss: 3,
+    };
+    notificationAlert.current.notificationAlert(options);
+}
   // check value data
-  const { register, handleSubmit } = useForm({
+  const { register, errors, handleSubmit } = useForm({
     defaultValues:{
       fullName:"",
       email:"",
       phone:"",
       title:"",
       content:"",
-      isStatus:"1"
+      isStatus:"false"
     }
   });
-  const [dataForm, setDataForm] = useState();
   // lưu data
   const onSubmit = data => {
-    alert(JSON.stringify(data));
+    let dataContact = [];
+    let localContact = localStorage.getItem('localContact');
+    if(localContact!=null){
+      dataContact = JSON.parse(localContact);
+      dataContact.push(data);
+    }else{
+      dataContact.push(data);
+    }
+     localStorage.setItem('localContact',JSON.stringify(dataContact));
+     Alert("","Sen to admin success");
   };
   return (
     <>
+    <NotificationAlert ref={notificationAlert} />
       <div className="main-contact m-5">
         <Row>
           <Col md={6} sm={12}>
@@ -41,24 +63,28 @@ export default function Contact() {
             <h2>Contact Us</h2>
             <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group ontrolId="customerName">
-                <Form.Control type="text" name="fullName" placeholder="Enter Name" ref={register} />
+                <Form.Control type="text" name="fullName" placeholder="Enter Name" ref={register({ required: true })} />
+                <p class="textvalidate"> {errors.fullName && "Name is required"} </p>
               </Form.Group>
               <Form.Group controlId="customerEmail">
-                <Form.Control type="email" name="email" placeholder="Enter email" ref={register} />
+                <Form.Control type="email" name="email" placeholder="Enter email" ref={register({ required: true })} />
+                <p class="textvalidate"> {errors.email && "Email is required"} </p>
               </Form.Group>
               <Form.Group controlId="customerPhone">
-                <Form.Control type="phone" name="phone" placeholder="Enter phone number" ref={register} />
+                <Form.Control type="phone" name="phone" placeholder="Enter phone number" ref={register({ required: true })} />
+                <p class="textvalidate"> {errors.phone && "Phone is required"} </p>
               </Form.Group>
               <Form.Group controlId="customerTitle">
-                <Form.Control type="text" name="title" placeholder="Enter Title" ref={register} />
+                <Form.Control type="text" name="title" placeholder="Enter Title" ref={register({ required: true })} />
+                <p class="textvalidate"> {errors.title && "Title is required"} </p>
               </Form.Group>
               <Form.Group controlId="ControlTextarea1">
-                <Form.Control as="textarea" name="content" placeholder="Enter Text" ref={register} />
+                <Form.Control as="textarea" name="content" placeholder="Enter Text" ref={register({ required: true })} />
+                <p class="textvalidate"> {errors.content && "Cotent is required"} </p>
               </Form.Group>
               <Form.Group hidden>
-                <Form.Control name="isStatus" ref={register}  value="1"/>
+                <Form.Control name="isStatus" ref={register}  />
               </Form.Group>
-              <p id="textValidate"></p>
               {/* <FormError isHidden={this.state.isInputValid} errorMessage={this.state.errorMessage}></FormError> */}
               <Button variant="success" type="submit">
                 Send <i class="fas fa-arrow-circle-right"></i>
